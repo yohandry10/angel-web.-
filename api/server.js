6cfc45ead21bee66289e393c6f0ddf55562b35e9
yohandrychirinos1 <yohandrychirinos1@gmail.com>
2026-05-19 08:55:15 -0500

    Normalize Request URL to absolute path for Vercel Serverless Function

--- api/server.js
+++ api/server.js
@@ -1,5 +1,13 @@
 import server from "../dist/server/server.js";
 
 export default async function handler(request) {
-  return server.fetch(request);
+  let absoluteUrl = request.url;
+  if (!absoluteUrl.startsWith("http://") && !absoluteUrl.startsWith("https://")) {
+    const host = request.headers.get("host") || "localhost";
+    const proto = request.headers.get("x-forwarded-proto") || "https";
+    absoluteUrl = `${proto}://${host}${request.url}`;
+  }
+  
+  const absoluteRequest = new Request(absoluteUrl, request);
+  return server.fetch(absoluteRequest);
 }
