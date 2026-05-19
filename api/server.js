a7caeb84158c9abedde26321f89400b88b3f0f17
yohandrychirinos1 <yohandrychirinos1@gmail.com>
2026-05-19 08:44:38 -0500

    Switch to standard Node.js serverless runtime for Vercel

--- api/server.js
+++ api/server.js
@@ -1,9 +1,5 @@
 import server from "../dist/server/server.js";
 
-export const config = {
-  runtime: "edge",
-};
-
 export default async function handler(request) {
   return server.fetch(request);
 }
